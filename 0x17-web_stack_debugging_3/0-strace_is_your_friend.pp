fbc3667a12a5d0d7dd36bf49ea6fdc0a1689f8b9
dannydms <shadaniels1997@gmail.com>
2023-08-10 22:24:14 -0700

    Tash liverpool

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Replacing right php config files to load php
+exec {'replacing_right path':
+  command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
