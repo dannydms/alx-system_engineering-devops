84d2e20e6b8499a01d83ad9b3a71b563736128b3
dannydms <shadaniels1997@gmail.com>
2023-08-10 22:38:34 -0700

    tasksalah

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Replacing right php config files to load php
-exec {'replacing_right path':
-  command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
+# Puppet manifest to fix a bug in wp-setings.php
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
